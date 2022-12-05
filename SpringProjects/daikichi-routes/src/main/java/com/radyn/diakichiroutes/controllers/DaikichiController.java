f0cd89969a0c6ce8731dd366a26561f7dc29bdc2
Radyn <radyn.mort22@gmail.com>
2022-12-04 21:50:45 -0800

    updated for daikichi path vars

--- SpringProjects/daikichi-routes/src/main/java/com/radyn/diakichiroutes/controllers/DaikichiController.java
+++ SpringProjects/daikichi-routes/src/main/java/com/radyn/diakichiroutes/controllers/DaikichiController.java
@@ -1,6 +1,7 @@
 package com.radyn.diakichiroutes.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,20 @@ public class DaikichiController {
 	@GetMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@GetMapping("/m/daikichi/travel/{location}")
+	public String travel(@PathVariable("location" ) String location) {
+		return "Congrats you will soon go to " + location;
+	}
+	
+	@GetMapping("/m/lotto/{number}")
+	public String loto(@PathVariable("number") Integer number) {
+		if(number%2 ==0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 	}
 }
